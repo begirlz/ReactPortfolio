dfd42ddc719c651fffdb89d6e2fd2822afa05c46
begirlz <sw.begirlz@gmail.com>
2023-03-11 06:34:24 -0700

    fix small screen

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,13 +3,13 @@ import portfolio from '../Portfolio.json'
 import ProjectCards from '../components/Project'
 
 function PortfolioWrapper(props) {
-    return <div className="d-flex flex-sm-wrap justify-content-center">{props.children}</div>;
+    return <div className="d-flex flex-sm-wrap justify-content-center flex-wrap">{props.children}</div>;
 }
 
 function Portfolio() {
     return (
         <section className="container-fluid col-lg-12 mb-5">
-            <div className="containe-fluid  d-flex flex-column">
+            <div className="containe-fluid d-flex flex-column">
                 <h2 className="card-title"><strong>Portfolio</strong></h2>
                 <hr style={{
                     backgroundColor: '#0390fc',
